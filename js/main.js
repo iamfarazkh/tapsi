7c5a8a154de2241697c8e3863316e06afa8baad1
iamfarazkh <farazgangkh@gmail.com>
2024-09-20 12:34:56 +0330

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const toggler = document.querySelector(".nav__toggler")
+const navbar = document.querySelector(".nav")
+toggler.addEventListener("click", (e) => {
+    navbar.classList.toggle("nav__expanded")
+})
+
+
+
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
+tabs.forEach((tab) => {
+    tab.addEventListener("click", (e) => {
+        const targetTabContent = document.querySelector(`#${tab.dataset.tabTarget}`);
+
+
+        tabs.forEach((tab) => tab.classList.remove("active"));
+        tabContents.forEach((tabContens) => tabContens.classList.remove("active"));
+
+
+        tab.classList.add("active");
+        targetTabContent.classList.add("active");
+    })
+});
